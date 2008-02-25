bf7e101e29a4f63254128be2bbbc83943d739a38
guillaume.garcera <guillaume.garcera@d9545162-2246-0410-accf-077c31a16dfc>
2008-02-25 09:59:44 +0000

    * finish trend in dashboard partie overview widget

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,12 @@
 module DashboardHelper
   def trends_for(data1, data2)
-    ""
+    if data1 < data2
+        tag = image_tag("up_g.gif")    
+    end
+    
+    if data1 > data2
+      tag = image_tag("down_r.gif")
+    end
+    tag
   end
 end
