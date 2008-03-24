22196224949b0785e3bd5baeee59afd4f9e9a786
guillaume.garcera <guillaume.garcera@d9545162-2246-0410-accf-077c31a16dfc>
2008-03-24 22:14:35 +0000

    * FIX attchment fu partinonning

--- app/models/game_photo.rb
+++ app/models/game_photo.rb
@@ -18,4 +18,8 @@ class GamePhoto < ActiveRecord::Base
   belongs_to :game
   has_attachment :storage => :file_system
   
+  #to convert old storage path with attchment fu partinionning system
+  def partitioned_path(*args)
+    args.unshift(id.to_s)
+  end
 end
