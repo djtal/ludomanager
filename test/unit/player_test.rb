b6285511045fb75ce512c4169c39799b64961181
djtal <guillaume.garcera@gmail.com>
2008-06-29 23:12:14 +0200

    FIX player

---
+++ test/unit/player_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class PlayerTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+  
+  
+  def test_should_not_exceed_party_game_max_player
+    
+  end
+end
