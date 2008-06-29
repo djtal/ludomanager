58593fb8e0917a7573411ade71d3f2e9bd9f4ea1
djtal <guillaume.garcera@gmail.com>
2008-06-29 18:10:51 +0200

    Use new hlepr for game box in all application [#25 state:resolved]

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,6 +1,7 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
+  helper :all
   include AuthenticatedSystem
   # If you want "remember me" functionality, add this before_filter to Application Controller
   before_filter :login_from_cookie
