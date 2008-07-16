e86df7ebe0ce93f7490ba451a640c3033ec9d176
djtal <guillaume.garcera@gmail.com>
2008-07-16 22:46:24 +0200

    Now dashboard partie widget is linked to partie calendar [#57 state:resolved]

--- app/models/game_photo.rb
+++ app/models/game_photo.rb
@@ -14,22 +14,6 @@
 #  game_id      :integer       
 #
 
-# == Schema Information
-# Schema version: 29
-#
-# Table name: game_photos
-#
-#  id           :integer       not null, primary key
-#  content_type :string(255)   
-#  filename     :string(255)   
-#  size         :integer       
-#  parent_id    :integer       
-#  thumbnail    :string(255)   
-#  width        :integer       
-#  height       :integer       
-#  game_id      :integer       
-#
-
 class GamePhoto < ActiveRecord::Base
   belongs_to :game
   has_attachment :storage => :file_system
