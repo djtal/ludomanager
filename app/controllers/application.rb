dc4ee049a7d255258cacc14433c93df622c29238
djtal <guillaume.garcera@gmail.com>
2008-08-19 22:37:37 +0200

    Add HopTaod exception notifier

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,20 +3,14 @@
 class ApplicationController < ActionController::Base
   helper :all
   include AuthenticatedSystem
+  include HoptoadNotifier::Catcher
   # If you want "remember me" functionality, add this before_filter to Application Controller
   before_filter :login_from_cookie
   before_filter :set_section
   before_filter :get_account_games, :if => :logged_in?
   
   
-  def rescue_action_in_public(exception)
-    case exception
-    when ::ActionController::UnknownAction, ::ActionController::RoutingError
-      render :file => File.join("#{RAILS_ROOT}", "public", "404.rhtml"), :layout => true, :status => "404 not found"
-    else
-      render :file => File.join("#{RAILS_ROOT}", "public", "500.rhtml"), :layout => true, :status => "500 error"
-    end
-  end
+
   
   protected
   
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '54173cbfc399f8da4b095144315a02c6'
+end
