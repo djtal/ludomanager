5c70c75c23d62b063cfed5a66d0f1d708acc3673
djtal <guillaume.garcera@gmail.com>
2008-10-14 21:20:30 +0200

    Fxi some tests

--- test/functional/editions_controller_test.rb
+++ test/functional/editions_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class EditionsControllerTest < ActionController::TestCase
+  fixtures :all
+  
   def test_should_get_index
     get :index
     assert_response :success
@@ -21,23 +23,23 @@ class EditionsControllerTest < ActionController::TestCase
   end
 
   def test_should_show_edition
-    get :show, :id => editions(:one).id
+    get :show, :id => editions(:battlelore_fr).id
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :id => editions(:one).id
+    get :edit, :id => editions(:battlelore_fr).id
     assert_response :success
   end
 
   def test_should_update_edition
-    put :update, :id => editions(:one).id, :edition => { }
+    put :update, :id => editions(:battlelore_fr).id, :edition => { }
     assert_redirected_to edition_path(assigns(:edition))
   end
 
   def test_should_destroy_edition
     assert_difference('Edition.count', -1) do
-      delete :destroy, :id => editions(:one).id
+      delete :destroy, :id => editions(:battlelore_fr).id
     end
 
     assert_redirected_to editions_path
--- test/functional/editors_controller_test.rb
+++ test/functional/editors_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class EditorsControllerTest < ActionController::TestCase
+  fixtures :all
+  
   def test_should_get_index
     get :index
     assert_response :success
@@ -21,23 +23,23 @@ class EditorsControllerTest < ActionController::TestCase
   end
 
   def test_should_show_editor
-    get :show, :id => editors(:one).id
+    get :show, :id => editors(:ystari).id
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :id => editors(:one).id
+    get :edit, :id => editors(:ystari).id
     assert_response :success
   end
 
   def test_should_update_editor
-    put :update, :id => editors(:one).id, :editor => { }
+    put :update, :id => editors(:ystari).id, :editor => { }
     assert_redirected_to editor_path(assigns(:editor))
   end
 
   def test_should_destroy_editor
     assert_difference('Editor.count', -1) do
-      delete :destroy, :id => editors(:one).id
+      delete :destroy, :id => editors(:ystari).id
     end
 
     assert_redirected_to editors_path
