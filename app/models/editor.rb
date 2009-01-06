98c7f78099c24948d8e0e93bda6f9817e245d007
djtal <guillaume.garcera@gmail.com>
2009-01-06 19:53:18 +0100

    FIX test data for editions add validation for editor lang like editions

--- app/models/editor.rb
+++ app/models/editor.rb
@@ -12,6 +12,7 @@
 
 class Editor < ActiveRecord::Base
   validates_uniqueness_of :name
+  validates_inclusion_of :lang, :in => ::Ludomanager::ISOCODES, :allow_nil => true, :allow_blank => true
   
   has_one :logo, :as => :attachable, :class_name => "Asset"
   has_many :editions
