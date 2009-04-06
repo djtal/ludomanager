458f7291a05c84fa23c39937326cd2e78deacb9f
djtal <guillaume.garcera@gmail.com>
2009-04-06 23:18:17 +0200

    FIX error on authorship form FIX error that break all json based autocomplete

--- test/functional/authorships_controller_test.rb
+++ test/functional/authorships_controller_test.rb
@@ -2,7 +2,16 @@ require 'test_helper'
 
 class AuthorshipsControllerTest < ActionController::TestCase
 
-  def test_truth
-    assert true
+  context "a logged user" do
+    setup{ login_as :quentin}
+    should "GET new authorship form" do
+      get :new, :game_id => games(:coloreto).id
+      assert_response :success
+    end
+    
+    should "GET a new form fragment when requestion a new authors for a game via js" do
+      get :new_partial_form, :index => 1, :format => :js
+      assert_response :success
+    end
   end
 end
