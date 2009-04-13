7a99776ccd20e61cd4b91508aa08f584a9d59b74
djtal <guillaume.garcera@gmail.com>
2009-04-13 23:36:02 +0200

    better use of gem in test

--- config/environment.rb
+++ config/environment.rb
@@ -35,8 +35,6 @@ Rails::Initializer.run do |config|
   
   # See Rails::Configuration for more options
   config.gem 'mislav-will_paginate', :version => '~> 2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
-  config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
   config.gem "thoughtbot-paperclip", :lib => "paperclip", :source => "http://gems.github.com", :version => '2.2.7'
   config.gem "newrelic_rpm"
   
