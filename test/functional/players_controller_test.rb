c0eea1c2a5f1a105b9b4f2b72d40808b4c594f8d
djtal <guillaume.garcera@gmail.com>
2009-10-29 22:33:32 +0100

    FIX registering players for parties bugs

--- test/functional/players_controller_test.rb
+++ test/functional/players_controller_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
 
-class PlayersControllerTest < ActiveSupport::TestCase
+class PlayersControllerTest < ActionController::TestCase
   # Replace this with your real tests.
+  context "A logged user" do
+    setup{ login_as :quentin}
+    
+    context "creating new player for a party" do
+      setup do
+        p = Factory.create(:party, :account_id => accounts(:quentin).id)
+        get :new, :party_id => p.id
+      end
+      should_respond_with :success
+
+    end
+    
+  end
 
   context "registering player for parties" do
+    should "add player to party" do
+
+    end
+    
     should_eventually "register player for current parties and go to next party" do
       flunk
     end
