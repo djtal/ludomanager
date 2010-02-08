c6917f06eec9b03f0b2bb923c3004d826bc810a8
djtal <guillaume.garcera@gmail.com>
2010-02-08 19:20:59 +0100

    remove old un-loclisaed date element

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -68,49 +68,4 @@ module ApplicationHelper
       txt += "</div>"
       txt += "</div>"
     end
-    
-    def month_fr
-        ["", "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout",
-              "Septembre", "Octobre", "Novembre", "Decembre"]
-    end
-    
-    def day_fr
-     ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
-    end
-    
-    def month_name index
-      names = month_fr
-      names[index]
-    end
-    
-    def distance_of_time_in_words(from_time, to_time = 0, include_seconds = false)
-      from_time = from_time.to_time if from_time.respond_to?(:to_time)
-      to_time = to_time.to_time if to_time.respond_to?(:to_time)
-      distance_in_minutes = (((to_time - from_time).abs)/60).round
-      distance_in_seconds = ((to_time - from_time).abs).round
-
-      case distance_in_minutes
-        when 0..1
-          return (distance_in_minutes == 0) ? 'moins d\' minute' : '1 minute' unless include_seconds
-          case distance_in_seconds
-            when 0..4   then 'less than 5 seconds'
-            when 5..9   then 'less than 10 seconds'
-            when 10..19 then 'less than 20 seconds'
-            when 20..39 then 'half a minute'
-            when 40..59 then 'less than a minute'
-            else             '1 minute'
-          end
-
-        when 2..44           then "#{distance_in_minutes} minutes"
-        when 45..89          then 'environ 1 heure'
-        when 90..1439        then "environ #{(distance_in_minutes.to_f / 60.0).round} heures"
-        when 1440..2879      then '1 jour'
-        when 2880..43199     then "#{(distance_in_minutes / 1440).round} jours"
-        when 43200..86399    then 'environ 1 mois'
-        when 86400..525959   then "#{(distance_in_minutes / 43200).round} mois"
-        when 525960..1051919 then 'environ 1 an'
-        else                      "plus #{(distance_in_minutes / 525960).round} ans"
-      end
-    end
-    
 end
