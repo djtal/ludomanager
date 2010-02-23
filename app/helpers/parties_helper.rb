4062acfc78b6a048b86cb9f9ebe86190a72f2a75
djtal <guillaume.garcera@gmail.com>
2010-02-23 22:50:29 +0100

    make parties per game a little like ACGame page

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -1,8 +1,5 @@
 module PartiesHelper
  
- def last_play_date_table_cell(game)
-   time_ago_in_words(@last_played[game])
- end
  
  def  account_game_status_for(game)
    content_tag(:span, "", :class => "ss_sprite ss_flag_green") if account_have_game?(game)
