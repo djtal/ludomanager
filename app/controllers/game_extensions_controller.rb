bf9d79efa71b1521cbfef3d4fce8313cba96712e
djtal <guillaume.garcera@gmail.com>
2010-10-07 23:13:33 +0200

    WIP : allow adding a game as an extension for a base game need to work on some javascript now to create the autocomplete for extesion

--- app/controllers/game_extensions_controller.rb
+++ app/controllers/game_extensions_controller.rb
@@ -4,4 +4,24 @@ class GameExtensionsController < ApplicationController
     @base_game = Game.find(params[:game_id])
     @extension = @base_game.extensions.build
   end
+  
+  
+  def create
+    @extension = Game.find(params[:game][:id]) unless params[:game][:id].blank?
+    @base_game = Game.find(params[:game][:base_game_id]) unless params[:game][:base_game_id].blank?
+    if(@base_game && @extension)
+      @extension.base_game = @base_game
+      @extension.save
+      respond_to do |wants|
+        wants.html { redirect_to(game_path(@base_game))  }
+      end
+    else
+      respond_to do |wants|
+        wants.html do
+          @extension = @base_game.extensions.build
+          render :new
+        end
+      end
+    end
+  end
 end
--- test/functional/game_extensions_controller_test.rb
+++ test/functional/game_extensions_controller_test.rb
@@ -1,8 +1,47 @@
 require 'test_helper'
 
 class GameExtensionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context "adding extensions to game" do
+    setup do
+      @base = Factory.create(:game, :name => "Race for the galaxy")
+      @ext = Factory.create(:game, :name => "Rebel vs Imperium")
+    end
+    
+    context "on GET to :new" do
+      setup { get :new, :game_id => @base.id }
+
+      should_respond_with :success
+      should_assign_to(:base_game, :class => Game) {@base}
+      should_assign_to(:extension, :class => Game)
+      
+      should "setup exntesion with base_game_id" do
+        assert_equal @base.id, assigns(:extension).base_game_id
+      end
+    end
+    
+    context "on POST to :create" do
+      setup { post :create, :game => { :id => @ext.id, :base_game_id => @base.id } }
+      
+      should_respond_with :redirect
+      should_redirect_to("base game path") {game_path(@base)}
+      
+      should "update the base_game_id of the extension with game.id" do
+        assert_equal @base.id, @ext.reload.base_game_id
+      end
+    end
+    
+    context "on POST to :create with no id for extension" do
+      setup { post :create, :game => { :id => "", :base_game_id => @base.id } }
+      should_render_template :new
+      should_assign_to(:base_game, :class => Game){@base}
+      should_assign_to(:extension, :class => Game)
+      
+      should "setup extension with base_game_id" do
+        assert_equal @base.id, assigns(:extension).base_game_id
+      end
+    end
+    
+    
   end
 end
