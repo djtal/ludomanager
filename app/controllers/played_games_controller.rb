44dd73fb0154fc034edca403c55398e071bf62b3
djtal <guillaume.garcera@gmail.com>
2011-03-07 21:40:48 +0100

    improve show played games graph page

--- app/controllers/played_games_controller.rb
+++ app/controllers/played_games_controller.rb
@@ -1,25 +1,31 @@
 class PlayedGamesController < ApplicationController
+  subnav :parties
+  layout "simple"
   
   def show
     @game = Game.find(params[:id])
-    @parties = current_account.parties.find_all_by_game_id(@game.id, :order => "created_at ASC")
-    options = {
-      :game => @game
-    }.merge(current_account.parties.play_range(:game => @game))
-    @breakdown = current_account.parties.yearly_breakdown(options).values.flatten
-    year = @breakdown.size/12
-
-    @chart = Gchart.new(  :type => :bar,
-                          :size => '670x200', 
-                          :alt => "Evolution des parties par mois",
-                          :bg => {:color => 'efefef', :type => 'gradient'},
-                          :axis_with_labels => ['x', 'y'], 
-                          :axis_labels => [(1..12).map{|m| t('date.month_names')[m].first} * year], 
-                          :axis_range => [nil, [@breakdown.min, @breakdown.max, 1]],
-                          :data => @breakdown)
     
     respond_to do |format|
-      format.html
+      format.html do
+        @range = current_account.parties.play_range(:game => @game)
+        options = {
+          :game => @game
+        }.merge(@range)
+        data = current_account.parties.yearly_breakdown(options)
+        breakdown = data.values.flatten
+        year = breakdown.size/12
+        @chart = Gchart.new(  :type => :bar,
+                              :size => '670x200', 
+                              :alt => "Evolution des parties par mois",
+                              :bg => {:color => 'efefef', :type => 'gradient'},
+                              :axis_with_labels => ['x', 'y'], 
+                              :axis_labels => [(1..12).map{|m| t('date.month_names')[m].first} * year], 
+                              :axis_range => [nil, [breakdown.min, breakdown.max, 1]],
+                              :data => breakdown)
+        @yearly = data.inject([]) do |acc, stats|
+          acc << stats[1].sum
+        end  
+      end
     end
   end
   
