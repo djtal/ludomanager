940373e72699a44bd4bfad24df9c8c46cb2e0e08
djtal <guillaume.garcera@gmail.com>
2011-12-04 12:18:45 +0100

    FIX old paperclip interpolation deprecation warning

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 # Hold interpolations to use with paperclip interpolation system for name attachment when saved
 #
 
-Paperclip::Attachment.interpolations[:editor] = proc do |attachment, style|
+Paperclip.interpolates :editor do |attachment, style|
   attachment.instance.name.parameterize.underscore.to_s
 end
