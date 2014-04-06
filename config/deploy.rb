e2f0bd48812d951cbb893a7f86b9b821d7820c87
Guillaume Garcera <guillaume.garcera@gmail.com>
2014-04-06 17:18:11 +0200

    Update to really overwrite asset tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,17 @@
-namespace :assets do
-  namespace :precompile do
-    task :if_changed do
-      false
-    end
+namespace :rails do
+  namespace :assets do
+    namespace :precompile do
+      task :if_changed do
+        false
+      end
 
-    task :default do
-      # do nothing since we don't have asset pipeline in rails 2.3
+      task :precompile do
+        # do nothing since we don't have asset pipeline in rails 2.3
+      end
+
+      task :default do
+        # do nothing since we don't have asset pipeline in rails 2.3
+      end
     end
   end
 end
